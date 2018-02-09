3f5f32f8034d1c5d5b8f575aadef495b278c567e
Devendra Lad <devendralad@Devendras-MacBook-Pro-2.local>
2018-02-08 23:12:10 -0600

    login page completed

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -2,4 +2,22 @@ class LoginController < ApplicationController
 	def index
 		
 	end
+	def new
+
+	end
+	def create
+		# render plain: params[:post].inspect
+		@email = params[:post][:email]
+		@password = params[:post][:password]
+		# redirect_to about_url
+
+		if @email == "admin@admin"
+		   redirect_to about_url
+		else
+		   # redirect_to request.referrer
+		   flash[:error] = "There were errors..."
+		   render :action => :index # This displays the new form again
+		end
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,6 @@ Rails.application.routes.draw do
   root 'login#index'
   #Regular routes
   get 'about' => 'about#about'
+
+  resources :login
 end
